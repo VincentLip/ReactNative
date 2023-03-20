7670e54a386acfdf23e3ab38ea8f01394d7a4c64
Lipinski Vincent <lipinskivincent@gmail.com>
2023-03-20 09:07:01 +0100

    recette

--- recette/component/Recette.js
+++ recette/component/Recette.js
@@ -1,4 +1,4 @@
-import {Button,Pressable, StyleSheet, Text, View } from 'react-native'
+import {Button,FlatList,Image,Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { MEALS } from '../data/data'
 
@@ -6,21 +6,35 @@ import { MEALS } from '../data/data'
 export default function Recette({navigation , route}) {
 
     const idCategory = route.params.idCategory
+    const tmpRecette = []
     console.log(idCategory)
     function Find(id){
 
         MEALS.forEach(element => {
             if(id == element.categoryIds){
-                console.log(element)        
+                tmpRecette.push(element)
+                console.log(tmpRecette)        
             }        
         })
         
     }
     Find(idCategory)
+
+    
   return (
     <View>
         <Button onPress={()=>Find(idCategory)} title='test'></Button>
-
+        <FlatList data={tmpRecette} renderItem={(itemData) => {
+        return (
+            <View>
+                <Image source={itemData.item.imageUrl} style={styles.image} resizeMode='contain' />
+                <Text>{itemData.item.title}</Text>
+                <Text>{itemData.item.duration} {itemData.item.complexity} {itemData.item.affordability}</Text>
+            </View>
+        )
+      }} keyExtractor={(item,index) => {
+        return item.id
+      }}></FlatList>
     </View>
    
   )
